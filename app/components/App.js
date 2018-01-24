ca68c1fdd747226f13dc3422faeef1a574ee46a6
17tranap <32630433+17tranap@users.noreply.github.com>
2018-01-23 17:24:05 -0800

    Add files via upload

---
+++ app/components/App.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require("react-dom");
+var Component = React.Component;
+
+class App extends Component {
+	render() {
+		return(
+			<h1>Hello, world</h1>
+		);
+	}
+}
+
+
+module.exports = App;
